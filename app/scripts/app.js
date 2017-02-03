070265dd52e7a6fc54da4f351f2b67bdd14f3d4f
Gama Aguilar <gama@9104studios.com>
2017-02-02 22:32:02 -0800

    Routing and landing page static complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,21 @@
-var app = angular.module('Blocitoff', ['ui.router', 'firebase']);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            });
+    }
+        
+    angular
+        .module('Blocitoff', ['ui.router', 'firebase'])
+        .config(config);
+    
+})();
